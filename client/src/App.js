75fda2eed97e9d0e7cbf1cbecbe1d37888dc60d2
eliyas41 <fentaeliyas@gmail.com>
2024-01-28 16:07:16 +0300

    add main footer(above footer) section component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Footer from "../src/components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
