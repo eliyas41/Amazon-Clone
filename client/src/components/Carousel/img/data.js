e27fef18df9a27117411fc300fae00ea52b6e3f3
eliyas41 <fentaeliyas@gmail.com>
2024-02-08 15:39:50 +0300

    Rename carousel effect Images

--- client/src/components/Carousel/img/data.js
+++ client/src/components/Carousel/img/data.js
@@ -1,9 +1,10 @@
-import img1 from "./10001.jpg"
-import img2 from "./10002.jpg"
-import img3 from "./10003.jpg"
-import img4 from "./10004.jpg"
-import img5 from "./10005.jpg"
-import img6 from "./10006.jpg"
-import img7 from "./10007.jpg"
+import img1 from "./1.jpg";
+import img2 from "./2.jpg";
+import img3 from "./3.jpg";
+import img4 from "./4.jpg";
+import img5 from "./5.jpg";
+import img6 from "./6.jpg";
+import img7 from "./7.jpg";
+import img8 from "./8.jpg";
 
-export const img = [img1, img2, img3, img4, img5, img6, img7];
+export const img = [img1, img2, img3, img4, img5, img6, img7, img8];
