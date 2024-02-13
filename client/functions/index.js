8651c0881013e36803df1a7a9a408b78c6a47d7f
eliyas41 <fentaeliyas@gmail.com>
2024-02-13 17:35:34 +0300

    Deploying on netlify

---
+++ client/functions/index.js
@@ -0,0 +1,38 @@
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Success",
+  });
+});
+
+app.post("/payment/create", async (req, res) => {
+  const total = parseInt(req.query.total);
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    console.log(paymentIntent);
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(403).json({
+      message: "total payment must be greater than 0",
+    });
+  }
+});
+
+exports.api = onRequest(app);
--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -2,7 +2,11 @@ import { create } from "@mui/material/styles/createTransitions";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-90197/us-central1/api",
+  //local instance of firebase functions
+  // baseURL: "http://127.0.0.1:5001/clone-90197/us-central1/api",
+
+  //deployed version of amazon server on render.com
+  baseURL: "https://amazon-api-deploy-5j4b.onrender.com/",
 });
 
 export { axiosInstance };
