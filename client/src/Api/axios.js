1873d9ed9e212b6f3617e94b7279a80a78df867c
eliyas41 <fentaeliyas@gmail.com>
2024-02-13 23:07:31 +0300

    update axios.js

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,4 +1,3 @@
-import { create } from "@mui/material/styles/createTransitions";
 import axios from "axios";
 
 const axiosInstance = axios.create({
@@ -6,7 +5,7 @@ const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-90197/us-central1/api",
 
   //deployed version of amazon server on render.com
-  baseURL: "https://amazon-api-deploy-5j4b.onrender.com/",
+  baseURL: "https://amazon-api-deploy-pm1z.onrender.com",
 });
 
 export { axiosInstance };
