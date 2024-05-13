969aca33f0410ff37bca7dfe918c9cc16a86ca66
eliyas41 <fentaeliyas@gmail.com>
2024-05-13 21:48:53 +0300

    Update Firebase

--- client/functions/index.js
+++ client/functions/index.js
@@ -24,7 +24,7 @@ app.post("/payment/create", async (req, res) => {
       amount: total,
       currency: "usd",
     });
-    console.log(paymentIntent);
+    // console.log(paymentIntent);
     res.status(201).json({
       clientSecret: paymentIntent.client_secret,
     });
